0e57e317b726c7e6dfaeb275989d209179b82b19
yepsson <32198001+yepsson@users.noreply.github.com>
2017-11-28 22:29:58 +0100

    updates structure
    example function
    and structure for some more

--- script.js
+++ script.js
@@ -11,10 +11,48 @@ fetch("recipes.json")
     recipes = recipesJson;
 });
 
+
 $(document).ready(function () {
 
-   
+    loopRecipe();
+
+//function that loop recipe + ingredients in lists. examplecode
+function loopRecipe(){
+
+    $("#container").append("<ul class='list'></ul>")
+
+       
+            $.each(recipes.recept, function(i, val){
+                $(".list").append("<h3>" + val.recept + "</h3>")
+                $(".list").append("<li>" + val.ingrediens + "</li>")
+            });
+}
+
+
+// function that find matching recipes from 1 ingredient in the loopAllIngredient function
+function findRecipes(){
+
+
+
+
+
+}
+
+// loops out all ingredients in a list and show copies ex: milk x3 instead of milk 3 times
+function loopAllIngredients(){
+
+
+
+
+
+}
+
+//loops one targeted recipe and the ingredients
+function loopOnerecipe(){
+
+
+
+
+}
 
 });
-
-
