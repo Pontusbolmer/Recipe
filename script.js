16071701572aace7b07084052d242e2a3035bd24
yepsson <32198001+yepsson@users.noreply.github.com>
2017-11-29 01:33:31 +0100

    added base functions
    created functions to use with eachother.
    
    some is only ready to console.log();
    
    some will need html/css to be connected but they all should work properly.

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ fetch("recipes.json")
 $(document).ready(function () {
 
     loopRecipe();
+    loopAllIngredients();
+    loopOneRecipe();
 
 //function that loop recipe + ingredients in lists. examplecode
 function loopRecipe(){
@@ -39,18 +41,49 @@ function findRecipes(){
 }
 
 // loops out all ingredients in a list and show copies ex: milk x3 instead of milk 3 times
-function loopAllIngredients(){
+function loopAllIngredients() {
+    // Find all ingredients
+    var result = []
+    recipes.forEach(function(recipe) {
+      result.push(recipe.ingrediens)
+    })
+  
+   //merges all the arrays with ingredients to one big array
+    var ingredients = [].concat.apply([], result)
+  
+    // Return object of with ingredients as key and the count as value.
+    // Allows us to do several.lök to print 5 or several.senap to print 3
+    var several = {}
+    ingredients.forEach(function(ingredient) {
+      several[ingredient] = (several[ingredient] || 0) + 1
+    })
+  
+    console.log(several)
+    return several
+
+    var lista = key + " " + several[key] + "x";
+    console.log(lista);
+
+  }
+    
+
+    
+
+  
 
 
-
-
-
-}
 
 //loops one targeted recipe and the ingredients
 function loopOneRecipe(){
 
-
+    $("#container").append("<ul class='list2'></ul>")
+    
+           
+                $.each(recipes.recept, function(i, val){
+                    $(".list2").append("<h3>" + val.recept[0] + "</h3>")
+                    $(".list2").append("<li>" + val.ingrediens + "</li>")
+                    console.log(val.recept[0]);
+                });
 
 
 }
