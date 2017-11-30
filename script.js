2f8d2621a8ff714c685bcc2cc6f7619beeb913f5
MartinMyr <martin.myrmarker@gmail.com>
2017-11-30 19:29:07 +0100

    fix login

--- script.js
+++ script.js
@@ -259,26 +259,6 @@ function pepparkakor(){
 
 //Addera till inköpslistan SLUT//
 
-$(".jansons-ingredienser").show();
-
-
-        showStartPage();
-
-        $.each(recipes, function (i, val) {
-            if (val.recept === "janson") {
-                $(".jansons-ingredienser").append("<li>" + val.recept + "</li>")
-                $(".jansons-ingredienser").append("<li>" + val.ingrediens + "</li>")
-                $(".jansons-ingredienser").append("<li>" + val.image + "</li>")
-            }
-        });
-
-   
-
-    function showStartPage() {
-        $(".jansons-ingredienser").show();
-        
-
-    };
 
 
 
