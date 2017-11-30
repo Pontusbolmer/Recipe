7cdcfc2b855d689fa1ad366b18676f97b2e9c16c
Jonatan Josefsson <jonatanjosef@Jonatans-MacBook-Pro.local>
2017-11-30 14:29:58 +0100

    added Martin to JS

--- script.js
+++ script.js
@@ -79,5 +79,152 @@ else lista[value] = 1;
 $("#main").append("<ul></ul>");
 for(var key in lista) {
 $("#main ul").append("<li>" + key + " " + lista[key] + "x" + "</li>");
+
 }
+
+// Stänger Pontus
+
+//Adderar till inköpslistan
+$(".addJulskinka").click(function(){
+    julskinka();
+});
+$(".addJanson").click(function(){
+    janson();
+});
+$(".addKöttbullar").click(function(){
+    kötbullar();
+});
+$(".addKal").click(function(){
+    kal();
+});
+$(".addRödbetssallad").click(function(){
+    rödbetssallad();
+});
+$(".addSill").click(function(){
+    sill();
+});
+$(".addPrinskorv").click(function(){
+    prinskorv();
+});
+$(".addRisalamalta").click(function(){
+    risalamalta();
+});
+
+
+function julskinka(){
+
+    $.each(recipes, function(i, val){
+        if(val.recept === "julskinka" ){
+            $("#shoppingList").append("<h2>" + val.recept  + "</h2>" );
+            $.each(val.ingrediens, function(i, val){
+            $("#shoppingList").append("<li>" + val  + "</li>" );
+            $("#shoppingList").append("<button>" + "Delete"  + "</button>" );
+            $(".addJulskinka").hide();
+            
+            });
+            
+        }  
+    })
+};
+function janson(){
+    
+    $.each(recipes, function(i, val){
+        if(val.recept === "janson" ){
+            $("#shoppingList").append("<h2>" + val.recept  + "</h2>" );
+            $.each(val.ingrediens, function(i, val){
+            $("#shoppingList").append("<li>" + val  + "</li>" );
+            $("#shoppingList").append("<button>" + "Delete"  + "</button>" );
+            $(".addJanson").hide();
+            });
+        }    
+    })
+};
+function kötbullar(){
+        
+    $.each(recipes, function(i, val){
+        if(val.recept === "köttbullar" ){
+            $("#shoppingList").append("<h2>" + val.recept  + "</h2>" );
+            $("#shoppingList").append("<li>" + val.ingrediens  + "</li>" );
+            $("#shoppingList").append("<button>" + "Delete"  + "</button>" );
+            $(".addKöttbullar").hide();
+        }  
+    })
+}
+function kal(){
+    
+    $.each(recipes, function(i, val){
+        if(val.recept === "kål" ){
+            $("#shoppingList").append("<h2>" + val.recept  + "</h2>" );
+            $.each(val.ingrediens, function(i, val){
+            $("#shoppingList").append("<li>" + val  + "</li>" );
+            $("#shoppingList").append("<button>" + "Delete"  + "</button>" );
+            $(".addKal").hide();
+            });
+        }  
+    })
+}
+function rödbetssallad(){
+    
+    $.each(recipes, function(i, val){
+        if(val.recept === "rödbetssallad" ){
+            $("#shoppingList").append("<h2>" + val.recept  + "</h2>" );
+            $.each(val.ingrediens, function(i, val){
+            $("#shoppingList").append("<li>" + val  + "</li>" );
+            $("#shoppingList").append("<button>" + "Delete"  + "</button>" );
+            $(".addRödbetssallad").hide();
+            });
+        }  
+    })
+}
+function sill(){
+    
+    $.each(recipes, function(i, val){
+        if(val.recept === "sill" ){
+            $("#shoppingList").append("<h2>" + val.recept  + "</h2>" );
+            $.each(val.ingrediens, function(i, val){
+            $("#shoppingList").append("<li>" + val  + "</li>" );
+            $("#shoppingList").append("<button>" + "Delete"  + "</button>" );
+            $(".addSill").hide();
+            });
+        }  
+    })
+}
+function prinskorv(){
+    
+    $.each(recipes, function(i, val){
+        if(val.recept === "prinskorv" ){
+            $("#shoppingList").append("<h2>" + val.recept  + "</h2>" );
+            $.each(val.ingrediens, function(i, val){
+            $("#shoppingList").append("<li>" + val  + "</li>" );
+            $("#shoppingList").append("<button>" + "Delete"  + "</button>" );
+            $(".addPrinskorv").hide();
+            });
+        }  
+    })
+}
+function risalamalta(){
+    
+    $.each(recipes, function(i, val){
+        if(val.recept === "risalamalta" ){
+            $("#shoppingList").append("<h2>" + val.recept  + "</h2>" );
+            $.each(val.ingrediens, function(i, val){
+            $("#shoppingList").append("<li>" + val  + "</li>" );
+            $("#shoppingList").append("<button class='deleteItem'>" + "Delete"  + "</button>" );
+            $(".deleteItem").click(function(){
+                var itemtoRemove = val.ingrediens;
+                recipes.splice($.inArray(itemtoRemove, arr),1);
+            });
+               
+          
+            $(".addRisalamalta").hide();
+            });
+        }  
+    })
+}
+
+
+//Addera till inköpslistan SLUT//
+
+
+
 });
